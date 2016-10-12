e8d373db00952390bd10ad4f512bddc8cec7197f
Soha Midha <soha.midha@iic.ac.in>
2016-10-12 16:28:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,8 +94,6 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var counter=0;
-
 app.get(`/count`, function (req, res) {
     counter=counter+1;
    res.send(counter.toString());
