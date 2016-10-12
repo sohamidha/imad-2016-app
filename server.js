b9c82f5017e4714faa391b9e210c41ae298970ad
Soha Midha <soha.midha@iic.ac.in>
2016-10-12 16:30:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+   res.send(counter.toString());
+});
+
 var articles = {
     'article-one': {
       title: 'Article One | Soha Midha',
@@ -93,11 +99,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-   res.send(counter.toString());
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
