13b867707fbffd891839f1664fc8e3922bb84007
Soha Midha <soha.midha@iic.ac.in>
2016-10-12 16:19:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,15 +89,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
     res.send(counter.toString());
-});
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
